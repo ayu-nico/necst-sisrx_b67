72fe238ed71b2080be2f14f6384152fa0faa312a
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2020-08-20 20:18:15 +0900

    add

--- scripts/yfactor_with_powermeter_b6.py
+++ scripts/yfactor_with_powermeter_b6.py
@@ -21,10 +21,11 @@ class sis_iv(object):
 
 
     def __init__(self):
-        self.pub1 = rospy.Publisher("/dev/cpz340816/rsw0/ch2",Float64,queue_size=1)
+        self.pub1 = rospy.Publisher("/dev/cpz340816/rsw0/ch1",Float64,queue_size=1)
+        self.pub2 = rospy.Publisher("/dev/cpz340816/rsw0/ch2",Float64,queue_size=1)
         self.sub1 = rospy.Subscriber("/dev/cpz3177/rsw0/ch3",Float64,self.stock_data1)
         self.sub2 = rospy.Subscriber("/dev/cpz3177/rsw0/ch4",Float64,self.stock_data2)
-        self.sub3 = rospy.Subscriber("/dev/cpz3177/rsw0/ch10",Float64,self.stock_data3)
+        #self.sub3 = rospy.Subscriber("/dev/cpz3177/rsw0/ch10",Float64,self.stock_data3)
 
         #self.vol = np.nan
         #self.cur = np.nan
@@ -53,6 +54,7 @@ class sis_iv(object):
             in_vol = (initv+interval*i)/3
             data = in_vol
             self.pub1.publish(in_vol)
+            self.pub2.publish(in_vol)
             time.sleep(0.3)
             da.append(self.vol/0.2)
             da.append(self.cur/0.002)
@@ -61,6 +63,8 @@ class sis_iv(object):
             self.da_all.append(da)
             time.sleep(0.01)
         self.pub1.publish(0)
+        self.pub2.publish(0)
+
         #print((da_all[-1][1]-da_all[0][1])/(da_all[-1][0]-da_all[0][0])) 傾き
         #np.savetxt(self.path + "sis_iv_{}.txt".format(self.ut), np.array(self.da_all), delimiter=" ")
 
@@ -94,4 +98,4 @@ if __name__ == "__main__" :
     logger.start(file_name)
     ctrl.measure(initv,interval,repeat)
     logger.stop()
-    ctrl.plot()
+    #ctrl.plot()
